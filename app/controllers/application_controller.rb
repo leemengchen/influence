cedb3a47641ae976475543ee5b525855b28917b6
leemengchen <orange_2039@hotmail.com>
2016-09-28 18:30:08 +0800

    update marketers sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def budget
-    number_to_currency(number, :unit => "MYR$ ", :separator => ",", :delimiter => ".")
-  end
-    
+
 end
