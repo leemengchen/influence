007b4c6e1dca940cfeba1947d995bbee2ed4f601
Nik Muhammad Amin <nik.muhammad.amin@gmail.com>
2016-09-28 15:43:32 +0800

    Fix sign in modal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def resource_name
+    :influencer
+  end
+  helper_method :resource_name
+
+  def resource
+    @resource ||= Influencer.new
+  end
+  helper_method :resource
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:influencer]
+  end
 end
