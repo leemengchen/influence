202aae05a01be9b6bc91719d7a97adff3353bbef
Nik Muhammad Amin <nik.muhammad.amin@gmail.com>
2016-10-04 14:36:48 +0800

    Add seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Load jobs
+json_data = File.read("db/jobs.json")
+json_jobs = JSON.parse(json_data)
+json_jobs["jobs"].each do |attr_hash|
+  Job.create!(attr_hash)
+end
