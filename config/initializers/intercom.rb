ad8d015fd230974c731fbeac477ccfd7a91eb5b8
Tevanraj Elengoe <tevanraj@Tevanrajs-MacBook-Pro.local>
2016-10-06 17:21:33 +0800

    fixed terms n privacy - added figaro

--- config/initializers/intercom.rb
+++ config/initializers/intercom.rb
@@ -1,7 +1,7 @@
 IntercomRails.config do |config|
   # == Intercom app_id
   #
-  config.app_id = ENV["INTERCOM_APP_ID"] || "lohpk5ji"
+  config.app_id = ENV["INTERCOM_APP_ID"] || "ENV.fetch('INTERCOM_APP_ID')"
 
   # == Intercom session_duration
   #
